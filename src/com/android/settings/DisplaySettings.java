37b60444baa2fd022c178abf952e33413e009500
SuperDroidBond <superdroidbond@yahoo.com>
2020-01-08 13:33:36 +0700

    Settings: Introduce RGB Gradient Picker [2/2]
    Signed-off-by: Reza Adi Pangestu <rezaadipangestu5@gmail.com>
    Change-Id: I7a1551471a7d819928a77496c34b169d60a8ffca
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -22,6 +22,8 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.provider.SearchIndexableResource;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.preference.Preference;
 
 import com.android.settings.dashboard.DashboardFragment;
